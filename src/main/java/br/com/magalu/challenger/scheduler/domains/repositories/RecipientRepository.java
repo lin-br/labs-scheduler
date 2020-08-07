68ec3d5d80fb8a1f895b69a2bd63c65360340048
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-08-07 15:31:35 -0300

    Tornar cadastro do destinatário único

--- src/main/java/br/com/magalu/challenger/scheduler/domains/repositories/RecipientRepository.java
+++ src/main/java/br/com/magalu/challenger/scheduler/domains/repositories/RecipientRepository.java
@@ -1,9 +1,13 @@
 package br.com.magalu.challenger.scheduler.domains.repositories;
 
 import br.com.magalu.challenger.scheduler.domains.entities.Recipient;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipientRepository extends JpaRepository<Recipient, UUID> {}
+public interface RecipientRepository extends JpaRepository<Recipient, UUID> {
+
+  Optional<Recipient> findByRecipient(String recipient);
+}
--- src/main/java/br/com/magalu/challenger/scheduler/services/RecipientService.java
+++ src/main/java/br/com/magalu/challenger/scheduler/services/RecipientService.java
@@ -1,6 +1,5 @@
 package br.com.magalu.challenger.scheduler.services;
 
-
 import br.com.magalu.challenger.scheduler.domains.entities.Recipient;
 import br.com.magalu.challenger.scheduler.domains.repositories.RecipientRepository;
 import java.util.Optional;
@@ -14,9 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.MANDATORY)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 class RecipientService {
+
   private final RecipientRepository recipientRepository;
 
   public Optional<Recipient> addRecipient(String recipient) {
+    return this.recipientRepository
+        .findByRecipient(recipient)
+        .or(() -> this.saveRecipient(recipient));
+  }
+
+  private Optional<Recipient> saveRecipient(String recipient) {
     return Optional.of(
         this.recipientRepository.save(Recipient.builder().recipient(recipient).build()));
   }
